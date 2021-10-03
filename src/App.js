aa97672bb273fa94169f0b161b98f9e27a0227ab
nrsimonelli <nrsimonelli@gmail.com>
2021-10-02 22:55:44 -0500

    added some default styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import { styled } from './stitches.config';
+
+const Container = styled('div', {
+  backgroundColor: '$pink',
+});
+
+const Button = styled('div', {
+  backgroundColor: '$red',
+  color: '$white',
+  borderRadius: '$round',
+});
+
 const App = () => {
-  return <div>hello, welcome to my app!</div>;
+  return (
+    <Container>
+      hello, welcome to my app!
+      <Button>Button</Button>
+    </Container>
+  );
 };
 
 export default App;
